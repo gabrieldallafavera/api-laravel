173367869995f183df635f77481868b502556c3a
Gabriel Bolson Dalla Favera <dallafavera.gabriel@gmail.com>
2021-11-25 11:59:27 -0300

    Commit Inicial

--- app/Http/Interfaces/User/IUserLogin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Http\Interfaces\User;
-
-interface IUserLogin
-{
-    public function login(array $fields): array;
-}
--- app/Http/Interfaces/User/IUserLogout.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Http\Interfaces\User;
-
-interface IUserLogout
-{
-    public function logout(): void;
-}
