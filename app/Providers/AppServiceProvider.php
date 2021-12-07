548c1facee5867bea5abd7d34f62de7a4cbe9c56
Gabriel Bolson Dalla Favera <dallafavera.gabriel@gmail.com>
2021-12-07 14:37:42 -0300

    Correção das interfaces

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,16 @@
 
 namespace App\Providers;
 
+use App\Http\Interfaces\User\IUserFind;
+use App\Http\Interfaces\User\IUserStore;
+use App\Http\Interfaces\User\IUserLogin;
+use App\Http\Interfaces\User\IUserLogout;
+use App\Http\Interfaces\User\IUserRegister;
+use App\Repositories\User\UserFind;
+use App\Repositories\User\UserStore;
+use App\Services\User\UserLoginService;
+use App\Services\User\UserLogoutService;
+use App\Services\User\UserRegisterService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +33,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->setUser();
+    }
+
+    public function setUser()
+    {
+        $this->app->bind(IUserFind::class, UserFind::class);
+        $this->app->bind(IUserStore::Class, UserStore::class);
+        $this->app->bind(IUserLogin::class, UserLoginService::class);
+        $this->app->bind(IUserLogout::class, UserLogoutService::class);
+        $this->app->bind(IUserRegister::class, UserRegisterService::class);
     }
 }
